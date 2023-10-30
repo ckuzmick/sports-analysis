514fff74bae59f2ab213d71eaf8a00536235b852
ckuzmick <conrad.kuzmick@icloud.com>
2023-10-30 07:57:55 -0400

    stuff

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 'use client'
 import { LineChart, Line } from 'recharts';
 
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+const data = [
+  {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+  {name: 'Page A', uv: 450, pv: 2400, amt: 2400},
+  {name: 'Page A', uv: 320, pv: 2400, amt: 2400},
+  {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+];
 
 const renderLineChart =  () => (
-  <LineChart width={400} height={400} data={data}>
+  <LineChart className='p-20' width={400} height={400} data={data}>
     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
   </LineChart>
 );
