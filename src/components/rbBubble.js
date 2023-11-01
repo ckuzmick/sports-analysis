dea3ce796757ea7d46320f281e06bf662a9fe79a
ckuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-11-01 14:13:25 -0400

    rb

--- src/components/rbBubble.js
+++ src/components/rbBubble.js
@@ -8,8 +8,8 @@ const RBBubble = () => {
 
     useEffect(() => {
         const margin = { top: 30, right: 50, bottom: 50, left: 50 },
-            width = 460 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+            width = 500 - margin.left - margin.right,
+            height = 420 - margin.top - margin.bottom;
 
         const svg = d3.select(svgRef.current)
             .attr("width", width + margin.left + margin.right)
@@ -18,20 +18,28 @@ const RBBubble = () => {
             .append("g")
             .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-        d3.csv("https://raw.githubusercontent.com/ckuzmick/d3-file-hosting/main/2023wrdata.csv").then(data => {
+        d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv").then(data => {
 
         const x = d3.scaleLinear()
-            .domain([0, 100])
+            .domain([0, 12000])
             .range([0, width]);
         svg.append("g")
             .attr("transform", `translate(0, ${height})`)
             .call(d3.axisBottom(x));
 
         const y = d3.scaleLinear()
-            .domain([0, 80])
+            .domain([35, 90])
             .range([height, 0]);
         svg.append("g")
             .call(d3.axisLeft(y));
+
+        const z = d3.scaleLinear()
+            .domain([200000, 1310000000])
+            .range([4, 40]);
+
+        const colorBalls = d3.scaleOrdinal()
+            .domain(["Asia", "Europe", "Americas", "Africa", "Oceania"])
+            .range(d3.schemeSet2)
 
         const tooltip = d3.select("body")
             .append("div")
@@ -42,15 +50,16 @@ const RBBubble = () => {
             .selectAll("dot")
             .data(data)
             .join("circle")
-            .attr("cx", d => x(+d.Tgt))
-            .attr("cy", d => y(+d.Rec))
-            .attr("r", 3)
-            .style("fill", "#69b3a2")
+                .attr("class", "buubbles")
+                .attr("cx", d => x(+d.gdpPercap))
+                .attr("cy", d => y(+d.lifeExp))
+                .attr("r", d => colorBalls(d.continent))
+                .style("fill", "#69b3a2")
             .on("mouseover", function (event, d) {
                 tooltip.transition()
                     .duration(200)
                     .style("opacity", .9);
-                tooltip.html(`Player: ${d.Player} </br> Targets: ${d.Tgt} </br> Receptions: ${d.Rec}`)
+                tooltip.html(`Player: ${d.zPlayer} </br> Targets: ${d.Tgt} </br> Receptions: ${d.Rec}`)
                     .style("left", (event.pageX + 10) + "px")
                     .style("top", (event.pageY - 28) + "px");
             })
