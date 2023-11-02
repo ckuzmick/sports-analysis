216318da2357b0b704bd610270110952c797b0f8
ckuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-11-02 10:23:47 -0400

    test

--- src/components/rbBubble.js
+++ src/components/rbBubble.js
@@ -9,8 +9,8 @@ const RBBubble = () => {
 
     useEffect(() => {
         const margin = { top: 30, right: 50, bottom: 50, left: 50 },
-            width = 500 - margin.left - margin.right,
-            height = 420 - margin.top - margin.bottom;
+            width = 600 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
 
         const svg = d3.select(svgRef.current)
             .attr("width", width + margin.left + margin.right)
