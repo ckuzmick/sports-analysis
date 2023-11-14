166defb1409ee5d5dd66ee74d52c3159ba0d847d
ckuzmick <conrad.kuzmick@icloud.com>
2023-11-13 20:20:19 -0500

    page-shift

--- src/app/page.js
+++ src/app/page.js
@@ -13,35 +13,37 @@ const homePage = () => (
   <main>
     <div className='grid'>
       <NavBar/>
-      <Wrapper>
-        <GraphLabel title='2023 WR Tgts vs. Recs' text="
-        Delve into the heart of football strategy with this chart, unveiling the intricate relationship between wide receivers' targets and successful catches. Through intuitive circles, players come to life on the chart, their sizes indicating salaries for swift comparisons. Essential for fans, analysts, and teams, this visualization sheds light on receiving skills, providing valuable insights for enthusiasts and data aficionados.
-        "/>
-        <WRScatter/>
-      </Wrapper>
-      <Wrapper>
-        <GraphLabel title='2022 RB Att vs. Yds' text='
-        This visualization captures the essence of football performance by depicting the correlation between rushing attempts and yards gained. Each player is represented by circles, their sizes reflecting salaries, enabling quick comparisons. Vital for fans, analysts, and teams, this chart unveils player dynamics, offering intriguing insights for enthusiasts and data aficionados alike.
-        '/>
-        <RBBubble22/>
-      </Wrapper>
-      <Wrapper>
-        <GraphLabel title='2023 RB Att vs. Yds' text='
-        Similar to the last graph, this showcases the abilities of different NFL running-backs compared to their compensation.
-        '/>
-        <RBBubble23/>
-      </Wrapper>
-      <Wrapper>
-        <GraphLabel title='Who&apos;s really titletown?' text='
-        Similar to the last graph, this showcases the abilities of different NFL running-backs compared to their compensation.
-        '/>
-        <CityChamps/>
-      </Wrapper>
-      <div className='flex place-self-center'>
-        <RegularSeasonPlayoffs2023/>
-      </div>
+      <h1 className='top-header'>Hi--this is my site for testing out d3, here&apos;s some of the things I&apos;ve done!!</h1>
     </div>
   </main>
 );
 
 export default homePage;
+
+{/* <Wrapper>
+<GraphLabel title='2023 WR Tgts vs. Recs' text="
+Delve into the heart of football strategy with this chart, unveiling the intricate relationship between wide receivers' targets and successful catches. Through intuitive circles, players come to life on the chart, their sizes indicating salaries for swift comparisons. Essential for fans, analysts, and teams, this visualization sheds light on receiving skills, providing valuable insights for enthusiasts and data aficionados.
+"/>
+<WRScatter/>
+</Wrapper>
+<Wrapper>
+<GraphLabel title='2022 RB Att vs. Yds' text='
+This visualization captures the essence of football performance by depicting the correlation between rushing attempts and yards gained. Each player is represented by circles, their sizes reflecting salaries, enabling quick comparisons. Vital for fans, analysts, and teams, this chart unveils player dynamics, offering intriguing insights for enthusiasts and data aficionados alike.
+'/>
+<RBBubble22/>
+</Wrapper>
+<Wrapper>
+<GraphLabel title='2023 RB Att vs. Yds' text='
+Similar to the last graph, this showcases the abilities of different NFL running-backs compared to their compensation.
+'/>
+<RBBubble23/>
+</Wrapper>
+<Wrapper>
+<GraphLabel title='Who&apos;s really titletown?' text='
+Similar to the last graph, this showcases the abilities of different NFL running-backs compared to their compensation.
+'/>
+<CityChamps/>
+</Wrapper>
+<div className='flex place-self-center'>
+<RegularSeasonPlayoffs2023/>
+</div> */}
---
+++ src/app/titletown/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import NavBar from '@/components/page/navBar';
+
+const homePage = () => (
+  <main>
+    <div className='grid'>
+      <NavBar/>
+      <h1 className='top-header'>Long have we debated the question of who&apos;s really titletown.<br/>This data analysis is going to answer that.</h1>
+    </div>
+  </main>
+);
+
+export default homePage;
